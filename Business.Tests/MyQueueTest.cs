4ac28a9ba9cab472f95298b104205235979ebcf8
Victoriia Tryfonova <v.tryfonova@sana-commerce.com>
2019-11-07 14:21:40 +0200

    MyQueue

---
+++ Business.Tests/MyQueueTest.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Business.Tests
+{
+    [TestClass]
+    public class MyQueueTest
+    {
+        [TestMethod]
+        public void MyQueue_Enqueue_OneItem()
+        {
+            var queue = new MyQueue<int>();
+            queue.Enqueue(231);
+
+            Assert.AreEqual(231, queue.Peek());
+        }
+
+        [TestMethod]
+        public void MyQueue_Enqueue_ManyItem()
+        {
+            var queue = new MyQueue<int>();
+            queue.Enqueue(45);
+            queue.Enqueue(89);
+            queue.Enqueue(34);
+            queue.Enqueue(8459);
+
+            Assert.AreEqual(45, queue.Peek());
+        }
+
+        [TestMethod]
+        public void MyQueue_Peek_TwoItems()
+        {
+            var queue = new MyQueue<int>();
+            queue.Enqueue(45);
+            queue.Enqueue(89);
+
+            Assert.AreEqual(45, queue.Peek());
+            Assert.AreEqual(89, queue.Peek());
+        }
+
+        [TestMethod]
+        public void MyQueue_Peek_ManyItems()
+        {
+            var queue = new MyQueue<int>();
+            queue.Enqueue(45);
+            queue.Enqueue(89);
+            queue.Enqueue(32);
+            queue.Enqueue(99);
+
+            Assert.AreEqual(45, queue.Peek());
+            Assert.AreEqual(89, queue.Peek());
+            Assert.AreEqual(32, queue.Peek());
+            Assert.AreEqual(99, queue.Peek());
+        }
+
+        [TestMethod, ExpectedException(typeof(InvalidOperationException))]
+        public void MyQueue_Peek_FromEmptyStack()
+        {
+            var queue = new MyQueue<int>();
+            queue.Peek();
+        }
+
+        [TestMethod, ExpectedException(typeof(InvalidOperationException))]
+        public void MyQueue_Clear()
+        {
+            var queue = new MyQueue<int>();
+            queue.Enqueue(45);
+            queue.Enqueue(89);
+            queue.Enqueue(32);
+            queue.Enqueue(99);
+
+            queue.Clear();
+            
+            queue.Peek();
+        }
+
+    }
+
+}
---
+++ Business/MyQueue.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business
+{
+    public class MyQueue<T>
+    {
+        class Item<U>
+        {
+            public U CurrentItem { get; set; }
+            public Item<U> NextItem { get; set; }
+        }
+
+        Item<T> firstItem;
+
+        public void Enqueue(T item)
+        {
+            if(firstItem == null)
+            {
+                firstItem = new Item<T> 
+                { 
+                    CurrentItem = item 
+                };
+                return;
+            }
+
+            var newItem = new Item<T> { CurrentItem = item };
+            var tmp = firstItem;
+
+            while (tmp.NextItem != null)
+            {
+                tmp = tmp.NextItem;
+            }
+
+            tmp.NextItem = newItem;
+
+        }
+
+        public T Peek()
+        {
+            if (firstItem == null)
+                throw new InvalidOperationException();
+
+            var res = firstItem.CurrentItem;
+
+            firstItem = firstItem.NextItem;
+
+            var tmp = firstItem;
+
+            while(tmp!=null)
+            {
+                tmp = tmp.NextItem;
+            }
+
+            tmp = new Item<T> { CurrentItem = res };
+
+
+            return res;
+
+        }
+
+        public void Clear()
+        {
+            firstItem = null;
+        }
+    }
+}
