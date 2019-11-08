7c628b66b4ae2da8970f0dece82a6920db75d02d
Victoriia Tryfonova <v.tryfonova@sana-commerce.com>
2019-11-08 11:33:16 +0200

    MyChiffreXOR

---
+++ Business.Tests/MyChiffreTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Business.Tests
+{
+    [TestClass]
+    public class MyChiffreTest
+    {
+        [TestMethod]
+        public void MyChiffre_Encode()
+        {
+            var chiffre = new MyChiffre();
+            var text = "Some text";
+            var keyWord = "burry";
+
+            Assert.AreNotEqual(text, chiffre.Encrypt(text, keyWord));
+        }
+
+        [TestMethod]
+        public void MyChiffre_Decode()
+        {
+            var chiffre = new MyChiffre();
+            var text = "Some text";
+            var keyWord = "burry";
+
+            chiffre.Encrypt(text, keyWord);
+
+            Assert.AreEqual(text, chiffre.Decrypt(keyWord));
+        }
+
+        [TestMethod]
+        public void MyChiffre_Encode_KeyWordLongerThenText()
+        {
+            var chiffre = new MyChiffre();
+            var text = "Some text";
+            var keyWord = "lackadaisical";
+
+            chiffre.Encrypt(text, keyWord);
+
+            Assert.AreEqual(text, chiffre.Decrypt(keyWord));
+        }
+    }
+    
+}
---
+++ Business/MyChiffre.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business
+{
+    public class MyChiffre
+    {
+        string textToEncrypt;
+
+        public string Encrypt(string text, string keyWord)
+        {
+            textToEncrypt = text;
+
+            return EncryptDecrypt(text, keyWord);
+        }
+
+        public string Decrypt(string keyWord) => EncryptDecrypt(textToEncrypt, keyWord);
+
+        string EncryptDecrypt(string encrypText, string keyWord)
+        {
+            var input = new StringBuilder(encrypText);
+            var output = new StringBuilder(encrypText.Length);
+
+            char tmp;
+
+            for (int i = 0; i < encrypText.Length; )
+            {
+                for (var j = 0; j < keyWord.Length && i < encrypText.Length; j++, i++)
+                {
+                    tmp = input[i];
+                    tmp = (char)(tmp ^ keyWord[j]);
+                    output.Append(tmp);
+                }
+            }
+
+            textToEncrypt = output.ToString();
+            return textToEncrypt;
+        }
+
+    }
+}
